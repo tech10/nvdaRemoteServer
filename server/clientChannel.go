b665b7837c44eb7eb31b938ee12c33f9d9607b3b
Blake G <blakeblind@gmail.com>
2021-03-24 09:04:44 -0600

    Send nvda_not_connected message if the client is a master, and no slaves are connected to control. The addon handles this, but the official server currently does not.

--- server/clientChannel.go
+++ server/clientChannel.go
@@ -79,6 +79,9 @@ func (c *ClientChannel) Add(client *Client) {
 	if encerr == nil {
 		client.Send(enc)
 	}
+	if len(clients) == 0 && connection == "master" {
+		client.Send([]byte("{\"type\":\"nvda_not_connected\"}"))
+	}
 	logstr := "Client " + strconv.Itoa(id) + " has joined channel " + c.name
 	if connection != "" {
 		logstr += " as a " + connection
