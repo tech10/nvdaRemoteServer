cb814ecba659ac5377fc8cf374da9b8dff92129e
Blake G <blakeblind@gmail.com>
2021-04-09 14:45:44 -0600

    Update certificate and include a bash script to automatically do this.

---
+++ update-cert.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# You need go installed to run this script.
+# It will regenerate the certificate included with this program.
+# If this takes a while, your system likely lacks entropy. See the readme for an explanation of this.
+go run . -launch=false -log-level=3 -gen-certfile ./cert.pem
