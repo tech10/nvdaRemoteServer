0c42526134ceada00e6990e179b532b1e412e457
Blake G <blakeblind@gmail.com>
2021-04-12 06:49:31 -0600

    Upgrade the bash script for upgrading the program.
    Signed-off-by: Blake G <blakeblind@gmail.com>

--- upgrade-nvdaremote.sh
+++ upgrade-nvdaremote.sh
@@ -15,11 +15,24 @@ echo Downloading.
 check wget -q https://github.com/tech10/nvdaRemoteServer/releases/latest/download/${program}.tar.gz
 echo Extracting.
 check tar -axf ${program}.tar.gz
+echo Checking new version.
+new_version=$(check ${program}/${binary} version)
+update() {
 echo Changing ownership of binary.
 check sudo chown root:root ${program}/${binary}
 echo Moving binary to /usr/bin
 check sudo mv ${program}/${binary} /usr/bin/
 echo Restarting server.
 check sudo systemctl restart nvdaRemoteServer
+}
+clean() {
 echo Cleaning up files.
 check rm -r ${program} ${program}.tar.gz
+}
+if [ "$version" = "$new_version" ]; then
+echo The two versions are identical. Not upgrading.
+clean
+exit
+fi
+echo Upgrading from $version to $new_version
+update
