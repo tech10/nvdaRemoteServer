2db78fcfc6940f908981257ded1542847e2a2b8e
Blake G <blakeblind@gmail.com>
2021-04-11 20:48:07 -0600

    Move file rewriting to its own function.

---
+++ server/file.go
@@ -0,0 +1,22 @@
+package server
+
+import (
+	"errors"
+	"os"
+)
+
+func file_rewrite(file string, data []byte) error {
+	w, err := os.OpenFile(file, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		return errors.New("Unable to create or open the file " + file + "\n" + err.Error())
+	}
+	_, err = w.Write(data)
+	if err != nil {
+		return errors.New("Unable to write to the file " + file + "\n" + err.Error())
+	}
+	err = w.Close()
+	if err != nil {
+		return errors.New("The file at " + file + " was unable to close. Information may not have been written to it correctly.\n" + err.Error())
+	}
+	return nil
+}
--- server/genCert.go
+++ server/genCert.go
@@ -9,7 +9,6 @@ import (
 	"crypto/x509/pkix"
 	"encoding/pem"
 	"math/big"
-	"os"
 	"time"
 )
 
@@ -82,21 +81,11 @@ func gen_cert_file(file string, cert, key []byte) {
 		return
 	}
 	Log(LOG_DEBUG, "Attempting to write certificate to file "+file)
-	w, err := os.OpenFile(file, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	err := file_rewrite(file, append(key, cert...))
 	if err != nil {
-		Log_error("Unable to create or open file for writing certificate information.\r\n", err.Error())
+		Log_error("Failed to write certificate.\n" + err.Error())
 		Launch_fail()
 		return
-	}
-	_, err = w.Write(append(key, cert...))
-	if err != nil {
-		Log_error("Unable to write to the file " + file + "\r\n" + err.Error())
-		Launch_fail()
-	}
-	err = w.Close()
-	if err != nil {
-		Log_error("The file at " + file + " was unable to close. Information may not have been written to it correctly.\r\n" + err.Error())
-		Launch_fail()
 	}
 	Log(LOG_DEBUG, "Certificate and key successfully written to "+file)
 }
