85271e89e79753ee806d09d422c0e64df36bedbe
Blake G <blakeblind@gmail.com>
2021-04-21 12:42:50 -0600

    Shell script for generating a configuration file.

---
+++ gen-config.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# Generate a sample configuration file.
+go run . -launch=false -gen-conf-file nvdaRemoteServer.json -cert cert.pem -key cert.pem -log-file nvdaRemoteServer.log
+rm nvdaRemoteServer.log
