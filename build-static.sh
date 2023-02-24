c97a3484726edf7d660c1fa7c97c23a9920639df
Blake G <blakeblind@gmail.com>
2023-02-24 10:37:49 -0700

    Explicitly enable CGO, in case it isn't enabled in a users default configuration.

--- build-static.sh
+++ build-static.sh
@@ -13,4 +13,4 @@
 # and trim the build path of your current working directory from all information returned in case of a panic.
 
 . ./functions.sh
-CC=musl-gcc go build -buildmode=pie -trimpath -ldflags "-linkmode external -w -s -extldflags '-static' -X main.Version=$(git_version)" -o nvdaRemoteServer .
+CGO_ENABLED=1 CC=musl-gcc go build -buildmode=pie -trimpath -ldflags "-linkmode external -w -s -extldflags '-static' -X main.Version=$(git_version)" -o nvdaRemoteServer .
